35ad47a0f795fddcde5c8e0919ebc076b6a5ca1c
Ritik Sharma <ritiksharma10508@gmail.com>
2026-01-16 12:02:31 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
+const radius = document.getElementById("radius").value
+const volume = document.getElementById("volume")
+const submit = document.getElementById("submit")
+
 function volume_sphere() {
     //Write your code here
-  
+	return (4/3) * Math.PI * Math.pow(radius,3)
 } 
+submit.addEventListener("click",()=>{
+	volume.value = volume_sphere()
+})
 
 window.onload = document.getElementById('MyForm').onsubmit = volume_sphere;
